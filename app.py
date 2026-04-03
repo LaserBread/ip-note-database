3991ecc2769a26e8d67ec66178b66c02e66cec39
Ethan <ej.laserbread@proton.me>
2026-04-02 18:47:31 -0700

    Fixed up app.py to use IPv6 Addresses (Stored Internally as Strings)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, Response, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String
+from sqlalchemy import LargeBinary
 from sqlalchemy.orm import Mapped, mapped_column
 from werkzeug.exceptions import BadRequest, NotFound
 import re
@@ -20,6 +20,7 @@ class Entry(db.Model):
     cidrmask: Mapped[int]
     mac: Mapped[str]
     notes: Mapped[str]
+    ipv6: Mapped[bytes] = mapped_column(LargeBinary(16), nullable=True)
 
 @app.route('/')
 def index():
@@ -28,7 +29,7 @@ def index():
 @app.route('/getall',methods=["GET"])
 def getall():
     result = Entry.query.all()
-    out = [{key: value for key, value in entry.__dict__.items() if key not in ['_sa_instance_state']} for entry in result]
+    out = [{key: value.hex() if isinstance(value, bytes) and key not in ['sa_instance_state'] else value for key, value in entry.__dict__.items() if key not in ['_sa_instance_state']} for entry in result]
 
     return jsonify(out)
 
@@ -37,13 +38,18 @@ def add():
     try:
         data = request.get_json()
 
+        ipv6_data = None
+        if 'ipv6' in data and data['ipv6'] is not None:
+            ipv6_data = bytes.fromhex(data['ipv6'])
+
         new_entry = Entry(
             name=data['name'],
             hostname=data.get('hostname'),
             ipv4=data.get('ipv4'),
             cidrmask=data.get('cidrmask'),
             mac=data.get('mac'),
-            notes=data.get('notes')
+            notes=data.get('notes'),
+            ipv6=ipv6_data
         )
 
         db.session.add(new_entry)
@@ -78,6 +84,14 @@ def update(entry_id):
             entry.ipv4 = data['ipv4']
         if 'cidrmask' in data:
             entry.cidrmask = data['cidrmask']
+        if 'ipv6' in data:
+            if data['ipv6'] is not None:
+                try:
+                    entry.ipv6 = bytes.fromhex(data['ipv6'])
+                except ValueError as e:
+                    raise BadRequest(f"Invalid IPv6 '{data['ipv6']}'.")
+            else:
+                entry.ipv6 = None
         if 'mac' in data:
             entry.mac = data['mac']
         if 'notes' in data:
